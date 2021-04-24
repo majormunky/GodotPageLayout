4857cb95a728fd534219b0d786ad5111d5cdff15
Josh Bright <josh.bright@gmail.com>
2021-04-24 02:32:28 -0700

    Added a file for util functions that gets autoloaded

---
+++ utils.gd
@@ -0,0 +1,23 @@
+extends Node
+
+
+var scale = 0.75
+
+
+func columns_to_inches(col, page_def):
+	if col == 1:
+		return page_def["column_width"]
+	else:
+		var result = 0
+		result += (page_def["column_width"] * col)
+		result += (page_def["gutter_width"] * (col - 1))
+		return stepify(result, 0.01)
+
+
+
+func to_inch(val):
+	return val / scale
+
+
+func from_inch(val):
+	return val * scale
